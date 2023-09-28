06fbb10a23e908d180c6c07a03a01704b93a16b2
Frankie <fwoodwar@gmail.com>
2023-09-28 11:29:16 -0600

    changes?

--- server.js
+++ server.js
@@ -27,14 +27,18 @@ let axiosInst = axios
 
 /* Configure the app to refresh the browser when nodemon restarts
 --------------------------------------------------------------- */
-const liveReloadServer = livereload.createServer();
-liveReloadServer.server.once("connection", () => {
-    // wait for nodemon to fully restart before refreshing the page
-    setTimeout(() => {
+// Detect if running in a dev environment
+if (process.env.ON_HEROKU === 'false') {
+    // Configure the app to refresh the browser when nodemon restarts
+    const liveReloadServer = livereload.createServer();
+    liveReloadServer.server.once("connection", () => {
+        // wait for nodemon to fully restart before refreshing the page
+        setTimeout(() => {
         liveReloadServer.refresh("/");
-    }, 100);
-});
-
+        }, 100);
+    });
+    app.use(connectLiveReload());
+}
 /* Configure the app (app.set)
 --------------------------------------------------------------- */
 app.set('view engine', 'ejs');
@@ -43,7 +47,7 @@ app.set('views', path.join(__dirname, 'views'));
 /* Middleware (app.use)
 --------------------------------------------------------------- */
 app.use(express.static('public'))
-app.use(connectLiveReload());
+
 // Body parser: used for POST/PUT/PATCH routes: 
 // this will take incoming strings from the body that are URL encoded and parse them 
 // into an object that can be accessed in the request parameter as a property called body (req.body).
